eec6ae018f808f4806a957439a93f3b0b0993a83
袁龙海 <yuanlonghai@sunlight.bz>
2018-10-25 13:19:56 +0800

    增加 抽象类

---
+++ J1Base/src/R10OOP_Asbtract/Chinese.java
@@ -0,0 +1,8 @@
+package R10OOP_Asbtract;
+
+public class Chinese extends Person {
+    @Override
+    public void run() {
+        System.out.println("ÖĞ¹úÈË¶ÌÅÜ");
+    }
+}
---
+++ J1Base/src/R10OOP_Asbtract/Person.java
@@ -0,0 +1,27 @@
+package R10OOP_Asbtract;
+
+// 抽象类无法实例化 只能子非抽象子类才可实例化
+public abstract class Person {
+
+    //公共方法
+    void speak(){
+         System.out.println("人类的话语我都会");
+     }
+
+     static void speak2(){
+        System.out.println("人类的话语我都会的 ——静态");
+    }
+
+     //公共字段  建议public  不然外界访问不到
+     private int age=18;
+
+    void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+  //抽象方法
+    public  abstract void  run();
+}
